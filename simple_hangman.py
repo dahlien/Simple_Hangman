014acb217c4be57130eda9e3bab73073cdb9ad9e
dahlien <48025492+dahlien@users.noreply.github.com>
2021-12-22 20:16:23 +0100

    Update
    * graphics of hangman added
    * user input with more than one character or no characters is not valid
    * user input with other character than a letter is not valid
    * invalid user inputs are not counted as failed attempts

--- simple_hangman.py
+++ simple_hangman.py
@@ -11,20 +11,126 @@ failed_attempts = 0
 
 while True:
     guessed_letter = (input("Guess a letter: ").lower())
-    if guessed_letter in secret_word:
-        position = secret_word.index(guessed_letter)
-        game_state = game_state[:position] + \
-            guessed_letter + game_state[position + 1:]
+    if len(guessed_letter) != 1:
+        print("Guess only one letter! \n")
     else:
-        failed_attempts += 1
-    print(game_state)
+        if not (ord(guessed_letter) >= 97 and ord(guessed_letter) <= 122):
+            print("This character is not a letter! \n")
+        else:
+            if guessed_letter in secret_word:
+                position = secret_word.index(guessed_letter)
+                game_state = game_state[:position] + \
+                    guessed_letter + game_state[position + 1:]
+            else:
+                failed_attempts += 1
+            print(game_state)
 
-    if "_" not in game_state:
-        print("Congratulations, you won!")
-        break
+            if "_" not in game_state:
+                print("Congratulations, you won!")
+                break
 
-    print(f"Number of failed attempts: {failed_attempts} \n")
+            print(f"Number of failed attempts: {failed_attempts} \n")
 
-    if failed_attempts >= 9:
-        print("You lose!")
-        break
+            if failed_attempts == 0:
+                print("""
+
+
+
+
+
+
+
+                ~~~~~~~
+                """)
+            elif failed_attempts == 1:
+                print("""
+                +
+                |
+                |
+                |
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 2:
+                print("""
+                +---.
+                |
+                |
+                |
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 3:
+                print("""
+                +---.
+                |   |
+                |
+                |
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 4:
+                print("""
+                +---.
+                |   |
+                |   O
+                |
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 5:
+                print("""
+                +---.
+                |   |
+                |   O
+                |   |
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 6:
+                print("""
+                +---.
+                |   |
+                |   O
+                | --|
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 7:
+                print("""
+                +---.
+                |   |
+                |   O
+                | --|--
+                |
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 8:
+                print("""
+                +---.
+                |   |
+                |   O
+                | --|--
+                |  /
+                |
+                ~~~~~~~
+                """)
+            elif failed_attempts == 9:
+                print("""
+                +---.
+                |   |
+                |   O
+                | --|--
+                |  / \\
+                |
+                ~~~~~~~
+                """)
+                print("You lose!")
+                break
